689f6ccef78d1e8de76166f9453e8163fb185915
Lelena1 <luppol@inbox.ru>
2022-11-14 22:55:13 +0300

    Сделано 1 и 2 задания из дз 2.1

--- Main.java
+++ Main.java
@@ -1,5 +1,14 @@
 public class Main {
     public static void main(String[] args) {
+        Human maksim = new Human(1988, "Максим", "Минск", "бренд-менеджер");
+        Human anya = new Human(1993, "Аня", "Москва", "методист образовательных программ");
+        Human katya = new Human(1992, "Катя", "Калининград", "продакт-менеджер");
+        Human artem = new Human(1995, "Артем", "Москва", "директор по развитию бизнеса");
+
+        maksim.greet();
+        anya.greet();
+        katya.greet();
+        artem.greet();
 
     }
 }
